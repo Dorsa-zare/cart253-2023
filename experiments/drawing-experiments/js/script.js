89cc8fc758c147bd03db3ff2a877d3d404768551
Dorsa-zare <D_zare@live.concordia.ca>
2023-09-06 10:51:44 -0400

    Update script.js

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -21,6 +21,24 @@ function preload() {
 */
 function setup() {
 
+    createCanvas(500,500);
+
+    background(255, 138, 200);
+
+    line(200,180,230,230);
+    line(300,180,265,230);
+
+    ellipse(250,270,120,120);
+
+    ellipse(200,180,20,20);
+    ellipse(300,180,20,20);
+
+    
+
+    line(220,260,240,260);
+    line(260,260,280,260);
+
+    line(235,290,265,290);
 }
 
 
