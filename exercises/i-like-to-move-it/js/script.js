f9a94b81b848f85083338d7b009449020d77f485
Dorsa-zare <D_zare@live.concordia.ca>
2023-09-18 19:20:36 -0400

    E1: Modifying descriptions and comments

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -8,10 +8,10 @@
 "use strict";
 
 /**
- * Description of preload
+ * None
 */
 
-// background color
+// Background color
 let bg = {
     r: 140,
     g: 188,
@@ -54,7 +54,7 @@ let building = {
     b: 130
 },
 }
-
+// First building's roof
 let roof = {
     x1: 80, 
     y1: 350, 
@@ -70,7 +70,7 @@ let roof = {
 }
 }
  
-// Second building (duplicate)
+// Second building 
 let building2 = {
     x: 400, 
     y: 380,
@@ -83,6 +83,7 @@ let building2 = {
     },
 }
 
+// Second building's roof
 let roof2 = {
     x1: 400,
     y1: 380,
@@ -98,7 +99,7 @@ let roof2 = {
 }
 
 
-// Third building (duplicate)
+// Third building 
 let building3 = {
     x: 230, 
     y: 450,
@@ -111,6 +112,7 @@ let building3 = {
     },
 }
 
+// Third building's roof
 let roof3 = {
     x1: 230,
     y1: 450,
@@ -134,77 +136,77 @@ function preload() {
 
 
 /**
- * Description of setup
+ * Creating Canvas
 */
 function setup() {
 createCanvas(800,600)
-noStroke();
+noStroke(); // Remove stroke
 }
 
 
 /**
- * Description of draw()
+ * Drawing three buildings, the sun and a moving cloud
 */
 function draw() {
     
-     //Drawing the Sun
+     // Drawing the Sun
 
     // Maping the sun's x and y position to mouse movement
     circle.x = map(mouseX, 0, width, -50, width + 50);
     circle.y = map(mouseY, 0, height, 50, height - 50);
-    // Map background color darkness to sun's y position
+    // Maping background color darkness to sun's y position
     let darkness = map(circle.y, 50, height - 50, 0, 50);
     background(140 - darkness, 188 - darkness, 222 - darkness);
-   // Calculate the sun's size based on its position
+   // Calculating the sun's size based on its position
    let sunSize = map(circle.y,50,height - 50, 200, 50);
-    // Set the fill color for the sun
+    // Setting the fill color for the sun
     fill(circle.fill.r, circle.fill.g, circle.fill.b);
     // Drawing the sun
     ellipse(circle.x, circle.y, sunSize);
     
 
-    //Drawing building 1
+    // Drawing building 1
 
-     // Set the fill color for the rectangle of the building
+     // Setting the fill color for the rectangle of the building
      fill(building.fill.r, building.fill.g, building.fill.b);
-    //Drawing rectangle for the building
+    // Drawing rectangle for the building
     rect(building.x,building.y,building.w,building.h);
-    //set fill color for the roof of the building
+    // Setting fill color for the roof of the building
     fill(roof.fill.r,roof.fill.g,roof.fill.b);
-    //Drawing the roof
+    // Drawing the roof
    triangle (roof.x1,roof.y1,roof.x2,roof.y2,roof.x3,roof.y3);
 
 
 
-   //Drawing building 2
+   // Drawing building 2
 
-        // Set the fill color for the rectangle of the building
+        // Setting the fill color for the rectangle of the building
      fill(building2.fill.r, building2.fill.g, building2.fill.b);
      //Drawing rectangle for the building
      rect(building2.x,building2.y,building2.w,building2.h);
-     //set fill color for the roof of the building
+     // Setting fill color for the roof of the building
      fill(roof2.fill.r,roof2.fill.g,roof2.fill.b);
-     //Drawing the roof
+     // Drawing the roof
     triangle (roof2.x1,roof2.y1,roof2.x2,roof2.y2,roof2.x3,roof2.y3);
  
  
-   //Drawing building 3
+   // Drawing building 3
 
-        // Set the fill color for the rectangle of the building
+        // Setting the fill color for the rectangle of the building
         fill(building3.fill.r, building3.fill.g, building3.fill.b);
-        //Drawing rectangle for the building
+        // Drawing rectangle for the building
         rect(building3.x,building3.y,building3.w,building3.h);
-        //set fill color for the roof of the building
+        // Setting fill color for the roof of the building
         fill(roof3.fill.r,roof3.fill.g,roof3.fill.b);
-        //Drawing the roof
+        // Drawing the roof
        triangle (roof3.x1,roof3.y1,roof3.x2,roof3.y2,roof3.x3,roof3.y3);
     
 
-        //Drawing the cloud and moving it
+        // Drawing the cloud and moving it
         
-         // Move the cloud to the right
+         // Moving the cloud to the right
          cloud.x += cloud.speed;
-         // Set the fill color for the cloud
+         // Setting the fill color for the cloud
         fill(cloud.fill); 
         // Drawing the cloud
         ellipse(cloud.x, cloud.y, cloud.width, cloud.height);
