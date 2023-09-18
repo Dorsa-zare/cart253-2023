eb05b38dcdd0b0c22337f37523321a67be62a7ce
Dorsa-zare <D_zare@live.concordia.ca>
2023-09-17 21:23:34 -0400

    map and constrain

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -11,11 +11,11 @@
 let backgroundShade = 0;
 
 let circle = {
-    x: 250,
+    x: 0,
     y: 250,
     size: 100,
     speed:1,
-    fill: 0
+    fill: 255
 };
 
 /**
@@ -40,11 +40,10 @@ function setup() {
 function draw() {
  background(backgroundShade);
 
-circle.speed = random (-5,5);
-circle.x += circle.speed;
-circle.size = random(10,100);
+circle.x = circle.x + circle.speed;
+circle.x = constrain(circle.x,0,width);
 
- circle.fill = random(0,255);
+circle.fill = map(circle.x,0,width,0,255);
  fill(circle.fill);
  ellipse(circle.x, circle.y, circle.size);
 
