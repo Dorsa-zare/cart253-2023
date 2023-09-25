2164247c4bb8c811e79a198c59080e63d687f5a1
Dorsa-zare <D_zare@live.concordia.ca>
2023-09-25 15:35:34 -0400

    making circle

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Conditionals experiments
+ * Dorsa Zare
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -8,8 +8,19 @@
 
 "use strict";
 
+let backgroundShade = 0;
+
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    speed: 2
+}
+
+
+
 /**
- * Description of preload
+ * None
 */
 function preload() {
 
@@ -20,13 +31,21 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+createCanvas(500,500);
 }
 
 
-/**
- * Description of draw()
-*/
-function draw() {
 
+function draw() {
+background(backgroundShade)
+
+circle.x = circle.x + circle.speed;
+
+fill(255,255,255);
+
+if (!(circle.x < width/3)) {
+    fill(255,0,0);
+}
+
+ellipse(circle.x,circle.y,circle.size);
 }
