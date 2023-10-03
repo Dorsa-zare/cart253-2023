66234524df1764bacc4e5127d4577f4091c2e358
Dorsa-zare <D_zare@live.concordia.ca>
2023-10-02 20:49:06 -0400

    EX: Changed the program and its states

--- exercises/Exercise_Love/js/script.js
+++ exercises/Exercise_Love/js/script.js
@@ -1,260 +1,142 @@
-/**
- * Love, Actually
- * Dorsa Zare
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 3
+};
 
-"use strict";
+let circle2 = {
+  x: 0,
+  y: 0,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5
+};
 
-let girl;
-let boy;
-let girlImage;
-let boyImage;
-let ghostImage; // Variable to hold the ghost boy's image
-let soulmateImage; // Variable to hold the soulmate's image
-let state = "title"; // Can be title, simulation, love, ghost, inLove
-let ghosted = false; // Flag to track if the boy has been ghosted
-let soulmate;
-
-
-/**
- * Description of preload
-*/
-function preload() {
-    // Load the girl's image
-    girlImage = loadImage("assets/images/girl.png");
-    // Load the boy's image
-    boyImage = loadImage("assets/images/boy.png");
-    // Load the ghost boy's image
-    ghostImage = loadImage("assets/images/ghost.png");
-    // Load the soulmate image
-  soulmateImage = loadImage("assets/images/soulmate.png");
-
-  }
-  
-
-
-/**
- * Description of setup
-*/
+let state = `title`; // Can be: title, simulation, love, sadness
 
 function setup() {
-   createCanvas(500, 500);
-    girl = {
-      x: width / 2,
-      y: height / 2,
-      size: 100,
-    };
-    setupBoy();
+  createCanvas(500,500);
+  setupCircles();
+}
+
+function setupCircles() {
+  // Position circles separated from one another
+  // circle1.x = width / 3;
+  circle2.x = 2 * width / 3;
+
+  // Start circles moving in a random direction
+  // circle1.vx = random(-circle1.speed,circle1.speed);
+  // circle1.vy = random(-circle1.speed,circle1.speed);
+  // circle2.vx = random(-circle2.speed,circle2.speed);
+  // circle2.vy = random(-circle2.speed,circle2.speed);
+
+  circle2.vx = 0;
+  circle2.vy = random(0,circle2.speed);
+}
+
+
+
+
+function draw() {
+  background(0);
+
+  if (state === `title`) {
+    title();
   }
-
-function setupBoy() {
-    boy = {
-      x: 0,
-      y: random(height),
-      size: 100,
-      vx: random(-2, 2),
-      vy: random(-2, 2),
-    };
-}
-
-function setupSoulmate() {
-    soulmate = {
-      x: random(width),
-      y: random(height),
-      size: 100,
-    };
-}
-
-
-
-  function draw() {
-    background(0);
- 
-
-    if (state === "title") {
-        title();
-      } else if (state === "simulation") {
-        simulation();
-      } else if (state === "love") {
-        love();
-      } else if (state === "ghost") {
-        ghost();
-      } else if (state === "inLove") {
-        inLove();
-      }
+  else if (state === `simulation`) {
+    simulation();
   }
-
-
-  function title() {
-   
-    push();
-    textSize(40);
-    fill(200, 100, 100);
-    textAlign(CENTER, CENTER);
-    text("Find your soulmate", width / 2, height / 2 - 20 );
-    pop();
-
-    push();
-    textSize(20);
-    fill(255);
-    textAlign(CENTER, CENTER);
-    text("Press Enter to start", width / 2, height / 2 + 20 );
-    pop();
-  
-    // Check for Enter key press to transition to the next state
-    if (keyIsPressed && key === "Enter") {
-      state = "simulation";
-    }
+  else if (state === `love`) {
+    love();
+  }
+  else if (state === `sadness`) {
+    sadness();
+  }
 }
 
-  
-  function simulation() {
-    // Move the boy
-    move(boy);
-    checkOffscreen(boy);
-  
-    // Calculate the distance between the girl and boy
-    let d = dist(girl.x, girl.y, boy.x, boy.y);
-  
-    // Check if the girl is moving towards the boy
-    if (d < girl.size / 2 + boy.size / 2) {
-      if (boy.x > width / 2) {
-        // If the touch is in the right half, boy turns into a ghost
-        ghosted = true;
-        state = "ghost";
-      } else if (ghosted) {
-        // If the touch is in the left half and boy was previously ghosted, transition to the "love" state
-        state = "love";
-      }
-    } else {
-      // Map girl's position to mouse movement
-      girl.x = mouseX;
-      girl.y = mouseY;
-    }
-  
-    display();
-    if (ghosted) {
-      ghostedText();
-    }
+function title() {
+  push();
+  textSize(40);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text(`Find your soulmate`,width/2,height/2);
+  pop();
 }
 
-
-  function love() {
-    push();
-    textSize(30);
-    fill(255, 150, 150);
-    textAlign(CENTER, CENTER);
-    text("You found your soulmate!", width / 2, height / 2);
-    pop();
-
-    // Check for Enter key press to transition to the "inLove" state
-    if (keyIsPressed && key === "Enter") {
-      state = "inLove";
-      setupSoulmate();
-    }
-}
-function ghost() {
-    // Display the ghosted text
-    ghostedText();
-
-    // Check for Enter key press to transition to the "love" state
-    if (keyIsPressed && key === "Enter") {
-      state = "love";
-    }
+function simulation() {
+  move();
+  checkOffscreen();
+  checkOverlap();
+  display();
 }
 
-
-function inLove() {
-    // Move the girl towards the soulmate with arrow keys
-    if (keyIsDown(LEFT_ARROW)) {
-      girl.x -= 2;
-    } else if (keyIsDown(RIGHT_ARROW)) {
-      girl.x += 2;
-    } else if (keyIsDown(UP_ARROW)) {
-      girl.y -= 2;
-    } else if (keyIsDown(DOWN_ARROW)) {
-      girl.y += 2;
-    }
-
-    // Calculate the distance between the girl and soulmate
-    let d = dist(girl.x, girl.y, soulmate.x, soulmate.y);
-
-    // Check if the girl is close to the soulmate
-    if (d < girl.size / 2 + soulmate.size / 2) {
-      state = "inLove";
-    }
-
-    display();
+function love() {
+  push();
+  textSize(40);
+  fill(255,150,150);
+  textAlign(CENTER,CENTER);
+  text(`You found your soulmate!`,width/2,height/2);
+  pop();
 }
 
+function sadness() {
+  push();
+  textSize(40);
+  fill(150,150,255);
+  textAlign(CENTER,CENTER);
+  text(`You got ghosted!`,width/2,height/2);
+  pop();
+}
 
 function move() {
-       // entity.x += entity.vx;
-        // entity.y += entity.vy;
-    
-        // if (entity.x < 0) {
-        // entity.x = width;
-        // } else if (entity.x > width) {
-        // entity.x = 0;
-        // }
-    
-        // if (entity.y < 0) {
-        // entity.y = height;
-        // } else if (entity.y > height) {
-        // entity.y = 0;
-        // }
+  // Move the circles
+  // circle1.x = circle1.x + circle1.vx;
+  // circle1.y = circle1.y + circle1.vy;
 
+  circle1.x = mouseX;
+  circle1.y = mouseY;
 
-  // Move the boy
-  boy.x = boy.x + boy.vx;
-  boy.y = boy.y + boy.vy;
+  circle2.x = circle2.x + circle2.vx;
+  // circle2.x = random(0,500) + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
+}
 
-
-
-
-    }
-
-  
 function checkOffscreen() {
-    if (
-      boy.x < 0 || boy.x > width ||
-      boy.y < 0 || boy.y > height
-    ) {
-      setupBoy();
-    }
+  // Check if the circles have gone offscreen
+  if (isOffscreen(circle1) || isOffscreen(circle2)) {
+    state = `sadness`;
+  }
+}
+
+function isOffscreen(circle) {
+  if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function checkOverlap() {
+  // Check if the circles overlap
+  let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
+  if (d < circle1.size/2 + circle2.size/2) {
+    state = `love`;
+  }
 }
 
 function display() {
-    image(girlImage, girl.x - girl.size / 2, girl.y - girl.size / 2, girl.size, girl.size);
-    if (ghosted) {
-      image(ghostImage, boy.x - boy.size / 2, boy.y - boy.size / 2, boy.size, boy.size);
-    } else if (state === "inLove") {
-      image(soulmateImage, soulmate.x - soulmate.size / 2, soulmate.y - soulmate.size / 2, soulmate.size, soulmate.size);
-    } else {
-      image(boyImage, boy.x - boy.size / 2, boy.y - boy.size / 2, boy.size, boy.size);
-    }
+  // Display the circles
+  ellipse(circle1.x,circle1.y,circle1.size);
+  ellipse(circle2.x,circle2.y,circle2.size);
 }
 
-  
-function ghostedText() {
-    push();
-    textSize(40);
-    fill(255, 0, 0); // Red color for being ghosted
-    textAlign(CENTER, CENTER);
-    text("You just got ghosted", width / 2, height / 2);
-  
-    textSize(20);
-    fill(255);
-    text("Press Enter to find your soulmate again", width / 2, height / 2 + 50);
-    pop();
-  
-    // Check for Enter key press to restart the game
-    if (keyIsPressed && key === "Enter") {
-      ghosted = false; // Reset the ghosted flag
-      setupBoy(); // Reset the boy's position
-      state = "simulation"; // Transition back to the "simulation" state
-    }
+function mousePressed() {
+  if (state === `title`) {
+    state = `simulation`;
+  }
 }
--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -13,26 +13,21 @@ let boy;
 let girlImage;
 let boyImage;
 let ghostImage; // Variable to hold the ghost boy's image
-let soulmateImage; // Variable to hold the soulmate's image
-let state = "title"; // Can be title, simulation, love, ghost, inLove
+let state = "title"; // Can be title, simulation, love, ghost
 let ghosted = false; // Flag to track if the boy has been ghosted
-let soulmate;
 
 
 /**
  * Description of preload
 */
 function preload() {
-    // Load the girl's image
-    girlImage = loadImage("assets/images/girl.png");
-    // Load the boy's image
-    boyImage = loadImage("assets/images/boy.png");
-    // Load the ghost boy's image
-    ghostImage = loadImage("assets/images/ghost.png");
-    // Load the soulmate image
-  soulmateImage = loadImage("assets/images/soulmate.png");
-
-  }
+     // Load the girl's image
+     girlImage = loadImage("assets/images/girl.png");
+     // Load the boy's image
+     boyImage = loadImage("assets/images/boy.png");
+     // Load the ghost boy's image
+     ghostImage = loadImage("assets/images/ghost.png");
+ }
   
 
 
@@ -41,134 +36,116 @@ function preload() {
 */
 
 function setup() {
-   createCanvas(500, 500);
-    girl = {
-      x: width / 2,
-      y: height / 2,
-      size: 100,
-    };
-    setupBoy();
-  }
+  createCanvas(500, 500);
+  girl = {
+    x: width / 2,
+    y: height / 2,
+    size: 100,
+  };
+  setupBoy();
+}
 
 function setupBoy() {
-    boy = {
-      x: 0,
-      y: random(height),
-      size: 100,
-      vx: random(-2, 2),
-      vy: random(-2, 2),
-    };
+  boy = {
+    x: 0,
+    y: random(height),
+    size: 100,
+    vx: random(1, 2), // Random horizontal velocity
+    vy: random(-2, 2), // Random vertical velocity
+  };
 }
-
-function setupSoulmate() {
-    soulmate = {
-      x: random(width),
-      y: random(height),
-      size: 100,
-    };
-}
-
 
 
   function draw() {
-    background(0);
- 
+    background(133, 194, 214);
 
     if (state === "title") {
         title();
+      } else if (state === "instruction") {
+        instruction();
       } else if (state === "simulation") {
         simulation();
       } else if (state === "love") {
         love();
       } else if (state === "ghost") {
         ghost();
-      } else if (state === "inLove") {
-        inLove();
       }
-  }
+}
 
 
   function title() {
-   
     push();
     textSize(40);
-    fill(200, 100, 100);
+    fill(207, 95, 139);
     textAlign(CENTER, CENTER);
-    text("Find your soulmate", width / 2, height / 2 - 20 );
+    text("Find your soulmate", width / 2, height / 2 - 30);
     pop();
-
+  
     push();
-    textSize(20);
+    textSize(25);
     fill(255);
     textAlign(CENTER, CENTER);
-    text("Press Enter to start", width / 2, height / 2 + 20 );
+    text("Press Mouse to start", width / 2, height / 2 + 40);
     pop();
   
-    // Check for Enter key press to transition to the next state
-    if (keyIsPressed && key === "Enter") {
-      state = "simulation";
+    // Check for mouse press to transition to the "instruction" state
+    if (mouseIsPressed) {
+      state = "instruction";
     }
+  }
+
+
+
+function instruction() {
+  push();
+  textSize(30);
+  fill(207, 95, 139);
+  textAlign(CENTER, CENTER);
+  text(" Instructions ", width / 2, height - 400);
+  pop();
+
+  push();
+  textSize(17);
+  fill(255);
+  textAlign(CENTER,CENTER);
+  text("Move with the arrows on your keyboard to go to your soulmate.", width / 2, height / 2 - 20);
+  text("Remember if someone ghosts you, they're not your soulmate! ", width / 2, height / 2 + 20);
+  pop();
+
+  push();
+  textSize(30);
+  fill(207, 95, 139);
+  textAlign(CENTER, CENTER);
+  text("Press Enter to start", width / 2, height - 100);
+  pop();
+
+  // Check for Enter key press to transition to the "simulation" state
+  if (keyIsPressed && key === "Enter") {
+      state = "simulation";
+  }
 }
 
-  
   function simulation() {
     // Move the boy
-    move(boy);
-    checkOffscreen(boy);
+    boy.x += boy.vx;
+    boy.y += boy.vy;
+  
+    // Check if the boy went out of the screen
+    if (boy.x > width) {
+      state = "ghost";
+      ghosted = true;
+      boy.x = width; // Ensure the boy is still visible on the screen
+    }
   
     // Calculate the distance between the girl and boy
     let d = dist(girl.x, girl.y, boy.x, boy.y);
   
-    // Check if the girl is moving towards the boy
+    // Check if the girl and boy touch
     if (d < girl.size / 2 + boy.size / 2) {
-      if (boy.x > width / 2) {
-        // If the touch is in the right half, boy turns into a ghost
-        ghosted = true;
-        state = "ghost";
-      } else if (ghosted) {
-        // If the touch is in the left half and boy was previously ghosted, transition to the "love" state
-        state = "love";
-      }
-    } else {
-      // Map girl's position to mouse movement
-      girl.x = mouseX;
-      girl.y = mouseY;
-    }
-  
-    display();
-    if (ghosted) {
-      ghostedText();
-    }
-}
-
-
-  function love() {
-    push();
-    textSize(30);
-    fill(255, 150, 150);
-    textAlign(CENTER, CENTER);
-    text("You found your soulmate!", width / 2, height / 2);
-    pop();
-
-    // Check for Enter key press to transition to the "inLove" state
-    if (keyIsPressed && key === "Enter") {
-      state = "inLove";
-      setupSoulmate();
-    }
-}
-function ghost() {
-    // Display the ghosted text
-    ghostedText();
-
-    // Check for Enter key press to transition to the "love" state
-    if (keyIsPressed && key === "Enter") {
       state = "love";
     }
-}
 
-
-function inLove() {
-    // Move the girl towards the soulmate with arrow keys
+    // Control the girl with arrow keys
     if (keyIsDown(LEFT_ARROW)) {
       girl.x -= 2;
     } else if (keyIsDown(RIGHT_ARROW)) {
@@ -178,83 +155,58 @@ function inLove() {
     } else if (keyIsDown(DOWN_ARROW)) {
       girl.y += 2;
     }
-
-    // Calculate the distance between the girl and soulmate
-    let d = dist(girl.x, girl.y, soulmate.x, soulmate.y);
-
-    // Check if the girl is close to the soulmate
-    if (d < girl.size / 2 + soulmate.size / 2) {
-      state = "inLove";
-    }
-
+  
     display();
 }
 
 
-function move() {
-       // entity.x += entity.vx;
-        // entity.y += entity.vy;
-    
-        // if (entity.x < 0) {
-        // entity.x = width;
-        // } else if (entity.x > width) {
-        // entity.x = 0;
-        // }
-    
-        // if (entity.y < 0) {
-        // entity.y = height;
-        // } else if (entity.y > height) {
-        // entity.y = 0;
-        // }
-
-
-  // Move the boy
-  boy.x = boy.x + boy.vx;
-  boy.y = boy.y + boy.vy;
-
-
-
-
-    }
-
-  
-function checkOffscreen() {
-    if (
-      boy.x < 0 || boy.x > width ||
-      boy.y < 0 || boy.y > height
-    ) {
-      setupBoy();
-    }
-}
-
-function display() {
-    image(girlImage, girl.x - girl.size / 2, girl.y - girl.size / 2, girl.size, girl.size);
-    if (ghosted) {
-      image(ghostImage, boy.x - boy.size / 2, boy.y - boy.size / 2, boy.size, boy.size);
-    } else if (state === "inLove") {
-      image(soulmateImage, soulmate.x - soulmate.size / 2, soulmate.y - soulmate.size / 2, soulmate.size, soulmate.size);
-    } else {
-      image(boyImage, boy.x - boy.size / 2, boy.y - boy.size / 2, boy.size, boy.size);
-    }
-}
-
-  
-function ghostedText() {
+  function love() {
     push();
-    textSize(40);
-    fill(255, 0, 0); // Red color for being ghosted
+    textSize(30);
+    fill(207, 95, 139);
     textAlign(CENTER, CENTER);
-    text("You just got ghosted", width / 2, height / 2);
-  
-    textSize(20);
-    fill(255);
-    text("Press Enter to find your soulmate again", width / 2, height / 2 + 50);
+    text("You found your soulmate!", width / 2, height / 2);
     pop();
-  
+
     // Check for Enter key press to restart the game
     if (keyIsPressed && key === "Enter") {
-      ghosted = false; // Reset the ghosted flag
       setupBoy(); // Reset the boy's position
+      ghosted = false; // Reset the ghosted flag
       state = "simulation"; // Transition back to the "simulation" state
     }
+}
+
+
+function ghost() {
+     // Display the ghosted text
+     ghostedText();
+
+     // Check for Enter key press to restart the game
+     if (keyIsPressed && key === "Enter") {
+       setupBoy(); // Reset the boy's position
+       ghosted = false; // Reset the ghosted flag
+       state = "simulation"; // Transition back to the "simulation" state
+     }
+ }
+
+ function display() {
+  image(girlImage, girl.x - girl.size / 2, girl.y - girl.size / 2, girl.size, girl.size);
+  if (ghosted) {
+    image(ghostImage, boy.x - boy.size / 2, boy.y - boy.size / 2, boy.size, boy.size);
+  } else {
+    image(boyImage, boy.x - boy.size / 2, boy.y - boy.size / 2, boy.size, boy.size);
+  }
+}
+
+function ghostedText() {
+  push();
+  textSize(40);
+  fill(207, 95, 139); 
+  textAlign(CENTER, CENTER);
+  text("You just got ghosted! ", width / 2, height / 2);
+
+  textSize(20);
+  fill(255);
+  text("Press Enter to restart", width / 2, height / 2 + 50);
+  pop();
 }
