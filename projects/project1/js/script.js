26406c6390e4efe825e42e4c33680d3cee42ef95
Dorsa-zare <D_zare@live.concordia.ca>
2023-10-17 15:02:17 -0400

    P1: Made final modifications (fixed mouse image, reset function, plants,..)

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -2,12 +2,7 @@
  * Project 1
  * Dorsa zare
  * 
- 
- * Organize codes
- * Fix offset for cleaning simulation
-  when restarting cant play the same game 
- * fix plant drawing per frame
- * 
+ * This is a game that will help you practice your skills for saving the environment. 
  * 
  */
 
@@ -93,25 +88,19 @@ function preload() {
      // Load bird image
     birdImage = loadImage("assets/images/bird.png");  // Load the bird image
 
-    //Trash1 image and its position
+    //Trash images and their position
     trashImages.push(loadImage("assets/images/trash1.png"));
     trashPositions.push({ x: 150, y: 200 });
-    //Trash2 image and its position
     trashImages.push(loadImage("assets/images/trash2.png"));
    trashPositions.push({ x: 350, y: 300 });
-   //Trash3 image and its position  
    trashImages.push(loadImage("assets/images/trash3.png"));
    trashPositions.push({ x: 500, y: 450 });
-    //Trash4 image and its position  
     trashImages.push(loadImage("assets/images/trash4.png"));
    trashPositions.push({ x: 560, y: 280 });
-   //Trash5 image and its position
    trashImages.push(loadImage("assets/images/trash5.png"));
    trashPositions.push({ x: 750, y: 430 });
-    //Trash6 image and its position
     trashImages.push(loadImage("assets/images/trash6.png"));
     trashPositions.push({ x: 950, y: 340 });
-    //Trash7 image and its position
     trashImages.push(loadImage("assets/images/trash7.png"));
     trashPositions.push({ x: 1050, y: 250 });
 
@@ -280,87 +269,44 @@ function draw() {
 
     function cleaning() {
         background(cleaningBackground);
-
-    // Hide the default cursor
-    // noCursor();
-
-    // Display bin
-    image(binImage, 80, 360, 200, 200);
+        noCursor();
+        image(binImage, 80, 360, 200, 200); // Display bin
 
     // Handle dragging behavior
     if (isDraggingTrash) {
         // Display the dragged trash image at the mouse position
-        image(trashImages[draggedTrashIndex], mouseX, mouseY, trashSize, trashSize);
+        // image(trashImages[draggedTrashIndex], mouseX, mouseY, trashSize, trashSize);
+        trashPositions[draggedTrashIndex].x = mouseX;
+        trashPositions[draggedTrashIndex].y = mouseY;
     }
-
      // Display each trash image at its specified position
     for (let i = 0; i < trashImages.length; i++) {
-        image(trashImages[i], trashPositions[i].x, trashPositions[i].y, trashSize, trashSize);
+        image(trashImages[i], trashPositions[i].x-trashSize/2, trashPositions[i].y-trashSize/2, trashSize, trashSize);
     }
-
      // Use the user's mouse position to display the hand image
-     image(handImage, mouseX, mouseY, 100, 100);
+     image(handImage, mouseX-50, mouseY-50, 100, 100);
 
-    
-    // Move the bird from right to left
-    bird.x -= bird.speed;
-
-    // Check if the bird has exited the canvas to the left
-    if (bird.x < -birdImage.width) {
-        // Reposition the bird at a random Y position and start from the right
-        bird.x = width;
-        bird.y = random(height);
-    }
-
-    // Display the bird image at the current position
-    image(birdImage, bird.x, bird.y, 200, 200);
+     displayBird(); //Display and move the bird image
 
 
-     if (trashImages.length === 0) {
-        // Transition to the "ending" state
+     if (trashImages.length === 0) {  // Transition to the "ending" state
         state = "cleaningEnding";
     }
-
-  
 }  
 
-
-function mousePressed() {
-    if (state === "cleaning" && !isDraggingTrash) {
-        // Check if the mouse is over a trash item
-        for (let i = 0; i < trashPositions.length; i++) {
-            let trash = trashPositions[i];
-            if (
-                mouseX > trash.x &&
-                mouseX < trash.x + trashSize &&
-                mouseY > trash.y &&
-                mouseY < trash.y + trashSize
-            ) {
-                isDraggingTrash = true;
-                draggedTrashIndex = i;
-                break;
-            }
-        }
-    }
+function displayBird() {
+      // Move the bird from right to left
+      bird.x -= bird.speed;
+      // Check if the bird has exited the canvas to the left
+      if (bird.x < -birdImage.width) {
+          // Reposition the bird at a random Y position and start from the right
+          bird.x = width;
+          bird.y = random(height);
+      }
+      // Display the bird image at the current position
+      image(birdImage, bird.x, bird.y, 200, 200);
 }
 
-function mouseReleased() {
-    if (isDraggingTrash) {
-        // Check if the dragged trash is over the bin
-        if (
-            mouseX > 80 &&
-            mouseX < 280 &&
-            mouseY > 360 &&
-            mouseY < 560
-        ) {
-            // Remove the trash item from the array
-            trashImages.splice(draggedTrashIndex, 1);
-            trashPositions.splice(draggedTrashIndex, 1);
-        }
-        isDraggingTrash = false;
-        draggedTrashIndex = -1;
-    }
-}
 
 function cleaningEnding() {
     // Display the ending screen
@@ -380,17 +326,12 @@ function cleaningEnding() {
       text("Press 'R' to restart the game.", width / 2, height / 2 + 170);
    
       if (keyIsPressed && key === 'r') {
+          reset();
+          cursor();
           state = "title"; // Transition back to the title page
       }
 }
 
-
-function keyPressed() {
-    if (keyCode === ENTER && state === "planting") {
-      // Set the flag to indicate Enter has been pressed
-      enterPressed = true;
-    }
-  }
 
   
   function plantingInstruction() {
@@ -425,59 +366,61 @@ function keyPressed() {
 
 
 function planting() {
- 
-    // Draw the planting background image as the background
-    image(plantingBackground, 0, 0, width, height);
 
-    // Draw the planting instruction at the top center of the screen
-  textSize(20);
-  fill(70, 110, 80);
-  textAlign(CENTER, CENTER);
-  text(plantingText, width / 2, 30);
+    image(plantingBackground, 0, 0, width, height);  // planting background image as the background
+    plantinginstructionText(); //Show planting text instruction
 
     // Draw the plant image at the current mouse position
     image(plantImage, mouseX - plantImage.width / 2 + 190 , mouseY - plantImage.height / 2 + 190 , 100, 100);
-  
-  if (mouseIsPressed) {
-    // Add the current mouse position to an array
-    plantedPlants.push({ x: mouseX, y: mouseY });
-  }
 
-  if (enterPressed) {
-    // Transition to the "plantingEnding" state when Enter is pressed
-    state = "plantingEnding";
-  }
-  
+    if (enterPressed) {  // Transition to the "plantingEnding" state when Enter is pressed
+        state = "plantingEnding";
+    }
   // Loop through the array of plant positions and draw the plant image at each position
   for (let plantPosition of plantedPlants) {
     image(plantImage, plantPosition.x - plantImage.width / 2 + 190, plantPosition.y - plantImage.height / 2 + 190, 100, 100);
   }
+    displayWater(); //display the water image 
+    moveWater(); //move the water
+    checkIfWaterExited(); // check if water exited canvas then reposition
+  }
 
-  // Draw the water image at the current position
+
+
+  function plantinginstructionText() {  
+    // Write the planting instruction at the top 
+    textSize(20);
+    fill(70, 110, 80);
+    textAlign(CENTER, CENTER); 
+    text(plantingText, width / 2, 30); 
+}
+
+function displayWater() {
   image(waterImage, water.x, water.y, 100, 100);
+}
 
-  // Move the water from right to left
-  water.x -= water.speed;
+function moveWater () {
+  water.x -= water.speed; // Move from right to left
+}
 
-  // Check if the water has exited the canvas to the left
+function checkIfWaterExited() {
+    // Check if the water has exited the canvas to the left
   if (water.x < -100) {
-      // Reposition the water at the right side of the canvas
-      water.x = width; // Outside the canvas
+    // Reposition the water at the right side of the canvas
+    water.x = width; 
+    // Update the Y position based on direction
+    if (water.direction === 1) {
+        water.y += 90; // Move down
+    } else {
+        water.y -= 90; // Move up
+    }
+    // Reverse direction if reaching minY or maxY
+    if (water.y >= water.maxY || water.y <= water.minY) {
+        water.direction *= -1; // Reverse direction
+        }
+    }
+}
 
-      // Update the Y position based on direction
-      if (water.direction === 1) {
-          water.y += 90; // Move down
-      } else {
-          water.y -= 90; // Move up
-      }
-
-      // Reverse direction if reaching minY or maxY
-      if (water.y >= water.maxY || water.y <= water.minY) {
-          water.direction *= -1; // Reverse direction
-      }
-  }
-    
-  }
 
   function plantingEnding () {
  // Display the ending screen
@@ -494,11 +437,84 @@ function planting() {
   Get out there and plant some trees, as our planet could use your green touch!
    `, width / 2, height / 2 + 50);
 
-
    fill(255);
    text("Press 'R' to restart the game.", width / 2, height / 2 + 150);
 
    if (keyIsPressed && key === 'r') {
-       state = "title"; // Transition back to the title page
+    enterPressed = false;   
+    reset();
+    state = "title"; // Transition back to the title page
    }
+  }
+
+
+
+  function reset() {
+    plantedPlants = [];
+//Trash images and their position
+trashImages.push(loadImage("assets/images/trash1.png"));
+trashPositions.push({ x: 150, y: 200 });
+trashImages.push(loadImage("assets/images/trash2.png"));
+trashPositions.push({ x: 350, y: 300 });
+trashImages.push(loadImage("assets/images/trash3.png"));
+trashPositions.push({ x: 500, y: 450 });
+trashImages.push(loadImage("assets/images/trash4.png"));
+trashPositions.push({ x: 560, y: 280 });
+trashImages.push(loadImage("assets/images/trash5.png"));
+trashPositions.push({ x: 750, y: 430 });
+trashImages.push(loadImage("assets/images/trash6.png"));
+trashPositions.push({ x: 950, y: 340 });
+trashImages.push(loadImage("assets/images/trash7.png"));
+trashPositions.push({ x: 1050, y: 250 });
+  }
+
+
+  function mousePressed() {
+    if (state === "cleaning" && !isDraggingTrash) {
+        // Check if the mouse is over a trash item
+        for (let i = 0; i < trashPositions.length; i++) {
+            let trash = trashPositions[i];
+            if (
+                mouseX > trash.x &&
+                mouseX < trash.x + trashSize &&
+                mouseY > trash.y &&
+                mouseY < trash.y + trashSize
+            ) {
+                isDraggingTrash = true;
+                draggedTrashIndex = i;
+                break;
+            }
+        }
+    }
+}
+
+function mouseReleased() {
+    if (state === "cleaning") {
+        if (isDraggingTrash) {
+            // Check if the dragged trash is over the bin
+            if (
+                mouseX > 80 &&
+                mouseX < 280 &&
+                mouseY > 360 &&
+                mouseY < 560
+            ) {
+                // Remove the trash item from the array
+                trashImages.splice(draggedTrashIndex, 1);
+                trashPositions.splice(draggedTrashIndex, 1);
+            }
+            isDraggingTrash = false;
+            draggedTrashIndex = -1;
+        }
+    }
+   
+    else if (state === "planting" ) {
+        plantedPlants.push({ x: mouseX, y: mouseY });
+    }
+}
+
+function keyPressed() {
+    if (keyCode === ENTER && state === "planting") {
+      // Set the flag to indicate Enter has been pressed
+      enterPressed = true;
+    }
   }
