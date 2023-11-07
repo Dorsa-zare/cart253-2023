c5ae9f805bd8629301f6ef2ca7b0c95ecd469c9d
Dorsa-zare <D_zare@live.concordia.ca>
2023-11-07 09:06:21 -0500

    P2: added hello state, created array for pet images

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -8,10 +8,8 @@
 
 "use strict";
 
-let petImage1;
-let petImage2;
-let petImage3;
-let petImage4;
+let petImages = [];
+let chosenPet;
 
 let state = "title";
 
@@ -19,11 +17,9 @@ let state = "title";
  * Description of preload
 */
 function preload() {
-    petImage1 = loadImage("assets/images/pet1.png");  
-    petImage2 = loadImage("assets/images/pet2.png");  
-    petImage3 = loadImage("assets/images/pet3.png");  
-    petImage4 = loadImage("assets/images/pet4.png");  
-  
+    for (let i = 1; i <= 4; i++) {
+        petImages[i] = loadImage(`assets/images/pet${i}.png`);
+    }
 }
 
 
@@ -39,92 +35,115 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-
     if (state === "title") {
         title();
-      } else if (state === "instruction") {
+    } else if (state === "instruction") {
         instruction();
-      } else if (state === "customizePet") {
+    } else if (state === "customizePet") {
         customizePet();
-      }
+    } else if (state === "hello") {
+        hello();
     }
+}
 
-
-    function title() {
-    // Display the title and "Press here to start" message
-    background(180,200,150);
+function title() {
+    // The title page
+    background(180, 200, 150);
     displayTitleText();
-    }
+}
 
-    function displayTitleText() {
-        textSize(50);
-        textAlign(CENTER, CENTER);
-        fill(0);
-        text("Emotional Support Pet", width / 2, height / 2 - 50);
-        textSize(20);
-        text("Press here to start", width / 2, height / 2 + 50);
-    }
+function displayTitleText() {
+    // Display the title and "Press here to start" message
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    fill(0);
+    text("Emotional Support Pet", width / 2, height / 2 - 50);
+    textSize(20);
+    text("Press here to start", width / 2, height / 2 + 50);
+}
 
-    function mousePressed() {
-        if (state === "title") {
-            // Transition to the instruction state when the user clicks on the title screen
-            state = "instruction";
-        } else if (state === "instruction") {
-            // Check if the user clicks on the "Press here to customize your pet" text
-            let customizePetButtonX = width / 2 - 150;
-            let customizePetButtonY = height / 2 + 90;
-            let customizePetButtonWidth = 300;
-            let customizePetButtonHeight = 40;
-    
-            if (mouseX > customizePetButtonX && mouseX < customizePetButtonX + customizePetButtonWidth &&
-                mouseY > customizePetButtonY && mouseY < customizePetButtonY + customizePetButtonHeight) {
-                // Transition to the customizePet state
-                state = "customizePet";
-            }
+function mousePressed() {
+    if (state === "title") {
+        state = "instruction";
+    } else if (state === "instruction") {
+        if (mouseY > height / 2 + 90) {
+            state = "customizePet";
         }
     }
+}
 
-      function instruction() {
-        background(180,200,150);
-        textSize(28);
-        textAlign(CENTER, CENTER);
-        fill(0);
-        text(`Have fun interacting with your virtual companion and
-         watch how it responds to your emotions. 
-         Don't forget to customize your pet's look to make it your own. 
-         Enjoy exploring the world of emotional well-being in this interactive experience!`, width / 2, height / 2 - 50);
-        textSize(20);
-        text("Press here to customize your pet", width / 2, height / 2 + 90);
-      }
+function instruction() {
+    background(180, 200, 150);
+    textSize(28);
+    textAlign(CENTER, CENTER);
+    fill(0);
+    text(`Have fun interacting with your virtual companion and
+        watch how it responds to your emotions.
+        Don't forget to customize your pet's look to make it your own.
+        Enjoy exploring the world of emotional well-being in this interactive experience!`, width / 2, height / 2 - 50);
+    textSize(20);
+    text("Press here to customize your pet", width / 2, height / 2 + 90);
+}
 
+function customizePet() {
+    background(180, 200, 150);
+    textSize(28);
+    textAlign(CENTER, CENTER);
+    fill(0);
+    text(` Choose your emotional support pet's look
+        to make it your own! `, width / 2, height / 2 - 200);
 
-      function customizePet() {
-        background(180,200,150);
-        textSize(28);
-        textAlign(CENTER, CENTER);
-        fill(0);
-        text(` Choose your emotional support pet's look
-         to make it your own! `, width / 2, height / 2 - 200);
-   
-         displayPetImages();
-         
-        }
+    displayPetImages();
+    displayPetLabels();
 
-            
-        function displayPetImages () {
-        // Display the pet images next to each other
-        image(petImage1, 140, height / 2 - 130, 300, 300);
-        image(petImage2, 400, height / 2 - 140, 320, 320);
-        image(petImage3, 620, height / 2 - 130, 300, 300);
-        image(petImage4, 850, height / 2 - 130, 300, 300);
-        
-        // Add a label for each pet image
-        textSize(18);
-        textAlign(CENTER, CENTER);
-        fill(0);
-        text("Pet 1", 340, height / 2 + 170);
-        text("Pet 2", 550, height / 2 + 170);
-        text("Pet 3", 770, height / 2 + 170);
-        text("Pet 4", 1000, height / 2 + 170);
+    checkChosenPet();
+}
 
-        }
+function displayPetImages() {
+    image(petImages[1], 140, height / 2 - 130, 300, 300);
+    image(petImages[2], 400, height / 2 - 140, 320, 320);
+    image(petImages[3], 620, height / 2 - 130, 300, 300);
+    image(petImages[4], 850, height / 2 - 130, 300, 300);
+}
+
+function displayPetLabels() {
+    // Add a label for each pet image
+    textSize(18);
+    textAlign(CENTER, CENTER);
+    fill(0);
+    text("Pet 1", 340, height / 2 + 170);
+    text("Pet 2", 550, height / 2 + 170);
+    text("Pet 3", 770, height / 2 + 170);
+    text("Pet 4", 1000, height / 2 + 170);
+}
+
+function checkChosenPet() {
+    if (mouseX > 140 && mouseX < 440 && mouseY > height / 2 - 130 && mouseY < height / 2 + 170) {
+        chosenPet = petImages[1];
+        state = "hello";
+    } else if (mouseX > 400 && mouseX < 720 && mouseY > height / 2 - 140 && mouseY < height / 2 + 180) {
+        chosenPet = petImages[2];
+        state = "hello";
+    } else if (mouseX > 620 && mouseX < 920 && mouseY > height / 2 - 130 && mouseY < height / 2 + 170) {
+        chosenPet = petImages[3];
+        state = "hello";
+    } else if (mouseX > 850 && mouseX < 1150 && mouseY > height / 2 - 130 && mouseY < height / 2 + 170) {
+        chosenPet = petImages[4];
+        state = "hello";
+    }
+}
+
+function hello() {
+    // Display the chosen pet image
+    background(180, 200, 150);
+    image(chosenPet, width / 2 - 150, height / 2 - 150, 300, 300);
+
+    // Display the text above the pet's head
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    fill(0);
+    text("Hello, I will be your emotional support pet", width / 2, height / 2 - 200);
+    textSize(20);
+    text("I will do my best to accompany you and help you in your journey.", width / 2, height / 2 - 160);
+
+}
