96b348e8d57ed1366c5fcb4777fc30dbd073e572
Dorsa-zare <D_zare@live.concordia.ca>
2023-11-25 12:34:49 -0500

    P2: Added the painting action by drawing squares in every frame

--- projects/project2/prototype/js/Painting.js
+++ projects/project2/prototype/js/Painting.js
@@ -2,11 +2,12 @@ class Painting {
     constructor(petCustomization) {
         this.state = "prompt";
         this.petCustomization = petCustomization;
-        this.backgroundImagePrompt = loadImage("assets/images/room.png"); //Background image for the prompt state
-        this.backgroundImagePainting = loadImage("assets/images/paintingbg.png"); // background image for painting
+        this.backgroundImagePrompt = loadImage("assets/images/room.png"); // Background image for the prompt state
+        this.backgroundImagePainting = loadImage("assets/images/paintingbg.png"); // Background image for painting
         this.buttonImage = loadImage("assets/images/button.png");
         this.rollerImage = loadImage("assets/images/roller.png");
         this.nextButtonText = "Next";
+        this.paintedSquares = [];
     }
 
     display() {
@@ -40,16 +41,37 @@ class Painting {
         // Display the new screen for the painting mini-game
         background(this.backgroundImagePainting);
         noCursor();
-        // Draw a pink square at the mouse position
+
+        // Draw previously painted squares
         fill(255, 186, 240); // Pink color
         noStroke();
-        rect(mouseX - 10, mouseY - 30, 70, 30);
+        for (let i = 0; i < this.paintedSquares.length; i++) {
+            let square = this.paintedSquares[i];
+            rect(square.x, square.y, 70, 30); // Adjust the size of the square as needed
+        }
+
+        // Draw a new pink square at the current mouse position
+        let newSquare = createVector(mouseX - 10, mouseY - 30);
+        rect(newSquare.x, newSquare.y, 70, 30);
+        this.paintedSquares.push(newSquare);
+
+        // Draw additional squares between the current and previous mouse positions
+        let numAdditionalSquares = 20; // number of additional squares
+        let previousPoint = createVector(pmouseX - 10, pmouseY - 30);
+        let currentPoint = createVector(mouseX - 10, mouseY - 30);
+
+        for (let i = 0; i < numAdditionalSquares; i++) {
+            let t = map(i, 0, numAdditionalSquares - 1, 0, 1);
+            let inBetweenX = lerp(previousPoint.x, currentPoint.x, t);
+            let inBetweenY = lerp(previousPoint.y, currentPoint.y, t);
+            rect(inBetweenX, inBetweenY, 70, 30);
+            this.paintedSquares.push(createVector(inBetweenX, inBetweenY));
+        }
 
         // Use the user's mouse position to display the roller image
         image(this.rollerImage, mouseX - 40, mouseY - 40, 150, 150);
     }
 
-    
     mousePressed() {
         // Check if the mouse is pressed on the "Next" button
         if (
@@ -65,6 +87,7 @@ class Painting {
                 // Handle the logic for changing the state to "painting"
                 this.state = "painting";
                 // Optionally, you can reset other game variables or perform other actions here
+                this.paintedSquares = []; // Clear previously painted squares when starting a new painting
             }
         }
     }
